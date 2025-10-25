353431c7301f350d7410089c6e92d2792b6a81c7
shahxvi <2025171523@student.uitm.edu.my>
2025-10-25 15:56:47 +0800

    Update nvim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,7 @@
+-- Variables --
+local tabsToSpaces = true
+local tabSize = 8
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
@@ -6,10 +10,10 @@ vim.opt.relativenumber = true
 vim.opt.number = true -- Show actual line number on highlight
 
 -- Tabs --
-vim.opt.expandtab = true -- Convert spaces to tabs
-vim.opt.tabstop = 8 -- Number of spaces that a tab character represents
-vim.opt.shiftwidth = 8 -- Number of spaces to use for each indentation
-vim.opt.softtabstop = 8 -- Number of spaces a tab keypress uses for indenting
+vim.opt.expandtab = tabsToSpaces -- Convert spaces to tabs
+vim.opt.tabstop = tabSize -- Number of spaces that a tab character represents
+vim.opt.shiftwidth = tabSize -- Number of spaces to use for each indentation
+vim.opt.softtabstop = tabSize -- Number of spaces a tab keypress uses for indenting
 
 -- Indents --
 vim.opt.autoindent = true
